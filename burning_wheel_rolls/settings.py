fcf1fe965893c513162f845c7fe3cd510b432bb7
Philippe Lemaire <philippe.lemaire@protonmail.com>
2022-02-15 15:38:14 +0100

    for static files

--- burning_wheel_rolls/settings.py
+++ burning_wheel_rolls/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
